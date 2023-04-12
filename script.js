52ef7dd249858981cf02e0ccf9025ebbcfa1eca5
skfarukhossain1998 <127541802+skfarukhossain1998@users.noreply.github.com>
2023-04-12 13:35:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
 function threeSum(arr, target) {
-arr.sort((a,b)=>a-b);
-	let ans=arr[0]+arr[1]+arr[2];
+if(arr.length < 3 || target == null) return;
+
+var arsort=arr.sort((a,b)=>a-b);
+
 var n=arr.length;
-	for (let i = 0; i < n-2; i++) {
-		let start=i+1;
-		let end=n-1;
+	var ans=arr[0]+arr[1]+arr[2];
+	for (var i = 0; i < n-2; i++) {
+		var start=i+1;
+		var end=n-1;
 		while (start<end) {
-			const sum=arr[0]+arr[start]+arr[end];
-			if (Math.abs(target-sum)<Math.abs[target-ans]) {
+			const sum=arsort[i]+arsort[start]+arsort[end];
+			if (Math.abs(sum - target) < Math.abs(ans - target)) {
 				ans=sum;
 			}
 			if (sum<target) {
 				start++;
-				
 			}
 			else
 			{
 				end--;
 			}
 		}
-		
 	}
 	return ans;
 }
+
 
 module.exports = threeSum;
